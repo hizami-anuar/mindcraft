ad5abfe3ec1fe7564d02908c27daa80a84ee89c3
hizami-anuar <hizami@mit.edu>
2020-01-15 11:12:57 -0500

    added mongoDB

--- server/server.js
+++ server/server.js
@@ -33,9 +33,9 @@ const socket = require("./server-socket");
 
 // Server configuration below
 // TODO change connection URL after setting up your team database
-const mongoConnectionURL = "FILL ME IN";
+const mongoConnectionURL = "mongodb+srv://MindCraft:1cuRPHmtCg3ZfsX6@mindcraft-y6b4z.mongodb.net/test?retryWrites=true&w=majority";
 // TODO change database name to the name you chose
-const databaseName = "FILL ME IN";
+const databaseName = "MindCraft";
 
 // connect to mongodb
 mongoose
