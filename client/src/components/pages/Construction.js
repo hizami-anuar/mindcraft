41ea87f546c366c0164294b2f61537fcbb5b7666
Ian Lee <leetinyau15@gmail.com>
2020-01-21 16:28:27 -0500

    updated home page

---
+++ client/src/components/pages/Construction.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import "./Skeleton.css";
+
+class Construction extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <br></br>   <br></br>
+        <h2><center>Site under construction. Thanks for visiting </center></h2>
+        <br></br>  <br></br> <br></br> 
+        <center><img src="https://gamepedia.cursecdn.com/minecraft_gamepedia/thumb/4/4d/Iron_Hoe.png/150px-Iron_Hoe.png?version=2cdedb8339795d911193d579cfa4b8ad"></img></center>
+      </div>
+    );
+  }
+}
+
+export default Construction;
