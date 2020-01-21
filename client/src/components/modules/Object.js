cd0e6cc0e0b0881490aa4b968595de5bd5b7b7a3
hizami-anuar <hizami@mit.edu>
2020-01-21 17:24:57 -0500

    Finished basic save/load functionality!!!

--- client/src/components/modules/Object.js
+++ client/src/components/modules/Object.js
@@ -11,7 +11,12 @@ class Object extends Component {
   render() {
     return (
       <>
-        <img id={this.props.objectId} className='draggable' src={this.props.imageURL}></img>
+        <img
+          id={this.props.objectId}
+          className='draggable'
+          src={this.props.imageURL}
+          style={{backgroundColor: 'black', left: this.props.x.toString()+'px', top: this.props.y.toString()+'px' }}
+        ></img>
       </>
     );
   }
