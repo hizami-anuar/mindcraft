f4c2f7390e8aab0a7a7df2979a6ea32a6be00959
hizami-anuar <hizami@mit.edu>
2020-01-20 21:35:06 -0500

    minor changes

--- server/server.js
+++ server/server.js
@@ -98,7 +98,8 @@ app.use((err, req, res, next) => {
   });
 });
 
-const port = process.env.PORT || 3000;
+port = process.env.PORT || 3000;
+
 const server = http.Server(app);
 socket.init(server);
 
