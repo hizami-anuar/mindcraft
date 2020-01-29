2371361ef8ec8766489eddad642b5d8f2ffc825b
umango101 <53319579+umango101@users.noreply.github.com>
2020-01-29 10:01:02 -0500

    minor changes

--- client/src/components/pages/Instructions.js
+++ client/src/components/pages/Instructions.js
@@ -15,12 +15,12 @@ class Instructions extends Component{
           <h2>To add a room to your house:</h2>
           <ol>
             <li>Click on house map</li>
-            <li>Choose "Add new room"</li>
+            <li>Choose <q>Add new room</q></li>
             <li>Click on the new room added</li>
             <li>Click on Background Select</li>
             <li>Select the type of room you want to create (Bedroom, Kitchen, Office, etc.)</li>
             <li>Select an option from the default backgrounds for your room</li>
-            <li>Click on "Create"</li>
+            <li>Click on <q>Create</q></li>
           </ol>
         </div>
         <div className="Instructions-changeBackground">
@@ -31,21 +31,21 @@ class Instructions extends Component{
             <li>Click on Background Select</li>
             <li>Select the type of room you want to create (Bedroom, Kitchen, Office, etc.)</li>
             <li>Select an option from the default backgrounds for your room</li>
-            <li>Click on "Create"</li>
+            <li>Click on <q>Create</q></li>
           </ol>
         </div>
         <div className="Instructions-addObjects">
           <h2>To add numbers and images to your room:</h2>
           <ol>
-            <li>Enter the Image URL and click on "Upload Image URL"</li>
-            <li>To view as an image, select "Image"; otherwise, select "Number"</li>
+            <li>Enter the Image URL and click on <q>Upload Image URL</q></li>
+            <li>To view as an image, select <q>Image</q> otherwise, select <q>Number</q></li>
           </ol>
         </div>
         <div className="Instructions-editObjects">
           <h2>To edit objects in your room:</h2>
           <ol>
             <li>Click on the object you want to edit</li>
-            <li>Select "Object" in "Log"</li>
+            <li>Select <q>Object</q> in <q>Log</q></li>
             <li>Choose which fields to edit by clicking on their respective Edit buttons</li>
           </ol>
         </div>
