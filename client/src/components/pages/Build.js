612fe04dd01c912e6341d131dbadcc76c59e3494
hizami-anuar <hizami@mit.edu>
2020-01-29 11:14:32 -0500

    improved functionality of several components

--- client/src/components/pages/Build.js
+++ client/src/components/pages/Build.js
@@ -100,6 +100,11 @@ class Build extends Component {
     return(
       <>
         <div className='Build-container'>
+        <button className='Build-button' onClick={() => this.setPanel('housemap')}>House Map</button>
+        <button className='Build-button' onClick={() => this.setPanel('backgroundselect')}>Select Background</button>
+        <button className='Build-button' onClick={() => this.setPanel('create')}>Create</button>
+
+        <button className='Build-button' onClick={this.loadHouse}>Load</button>
         {
         this.props.userId === undefined ? (
           <div>Please log in.</div>
@@ -139,11 +144,6 @@ class Build extends Component {
 
         (<div>Error</div>)
         }
-        <button className='Build-button' onClick={() => this.setPanel('housemap')}>HouseMap</button>
-        <button className='Build-button' onClick={() => this.setPanel('backgroundselect')}>BackgroundSelect</button>
-        <button className='Build-button' onClick={() => this.setPanel('create')}>Create</button>
-
-        <button className='Build-button' onClick={this.loadHouse}>Load</button>
         </div>
       </>
     )
