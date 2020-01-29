c461921b55d4ecb77907bf3f304f6db7aa53ae38
umango101 <53319579+umango101@users.noreply.github.com>
2020-01-29 11:16:41 -0500

    removed lists

--- client/src/components/pages/Instructions.js
+++ client/src/components/pages/Instructions.js
@@ -16,34 +16,26 @@ class Instructions extends Component{
         <div className='Instructions-addRoom'>
           <h2>To add a room to your house:</h2>
           <p>
-             Click on house map. Then, choose <q>Add new room</q>. Next, click on the new room you added. Click on Background Select. Then select the type of room you want to create (Bedroom, Kitchen, Office, etc.). Select an option from the default backgrounds for your room. Then, click on <q>Create</q>.
+             Click on house map. Then, choose <q>Add new room</q>. Next, click on the new room you added. Click on <q>Background Select</q>. Then select the type of room you want to create (Bedroom, Kitchen, Office, etc.). Select an option from the default backgrounds for your room. Then, click on <q>Create</q>.
           </p>
         </div>
         <div className='Instructions-changeBackground'>
           <h2>To change the background of an existing room:</h2>
-          <ol>
-            <li>Click on house map</li>
-            <li>Choose the room whose background you want to change</li>
-            <li>Click on Background Select</li>
-            <li>Select the type of room you want to create (Bedroom, Kitchen, Office, etc.)</li>
-            <li>Select an option from the default backgrounds for your room</li>
-            <li>Click on <q>Create</q></li>
-          </ol>
+          <p>
+            Click on house map. Then, choose the room whose background you want to change. Click on <q>Background Select</q>. Then, select the type of room you want to create (Bedroom, Kitchen, Office, etc.). Select an option from the default backgrounds for your room. Finally, click on <q>Create</q>. 
+          </p>
         </div>
         <div className='Instructions-addObjects'>
           <h2>To add numbers and images to your room:</h2>
-          <ol>
-            <li>Enter the Image URL and click on <q>Upload Image URL</q></li>
-            <li>To view as an image, select <q>Image</q> otherwise, select <q>Number</q></li>
-          </ol>
+          <p>
+            Enter the Image URL and click on <q>Upload Image URL</q>. To view the object as an image, select <q>Image</q>. Otherwise, select <q>Number</q>.
+          </p>
         </div>
         <div className='Instructions-editObjects'>
           <h2>To edit objects in your room:</h2>
-          <ol>
-            <li>Click on the object you want to edit</li>
-            <li>Select <q>Object</q> in <q>Log</q></li>
-            <li>Choose which fields to edit by clicking on their respective Edit buttons</li>
-          </ol>
+          <p>
+            Click on the object you want to edit. Select <q>Object</q> in <q>Log</q>. Choose which fields to edit by clicking on their respective Edit buttons.
+          </p>
         </div>
       </div>
       </>
