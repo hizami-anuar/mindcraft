a6c7d9920c6a0b5aa6bc0fc127f5c31f41b3e2ff
hizami-anuar <hizami@mit.edu>
2020-02-15 14:10:24 -0500

    Revert "Merge pull request #1 from hizami-anuar/hizami"
    This reverts commit 56da3f5ae91340786b93e5e2ac82f1070544c234, reversing
    changes made to 8cbef7dc7aedb2a574b0a093d06cb5b2b57630c6.

--- server/server.js
+++ server/server.js
@@ -17,6 +17,7 @@
 // this is a tool provided by staff, so you don't need to worry about it
 const validator = require("./validator");
 validator.checkSetup();
+require("dotenv").config();
 
 //import libraries needed for the webserver to work!
 const http = require("http");
@@ -98,9 +99,10 @@ app.use((err, req, res, next) => {
 });
 
 port = process.env.PORT || 3000;
+
 const server = http.Server(app);
 socket.init(server);
 
-server.listen(process.env.PORT || port, () => {
+server.listen(port, () => {
   console.log(`Server running on port: ${port}`);
 });
